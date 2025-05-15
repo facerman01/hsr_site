3e4404143b6e25f8a21cae9a04885c77fa405baf
facerman <tayjinheng@gmail.com>
2025-05-15 20:39:02 +0200

    added all image source files

--- hsr-site/src/Characters.jsx
+++ hsr-site/src/Characters.jsx
@@ -6,22 +6,351 @@ const characters = [
     image: "/units/acheron.webp",
     limited5Cost: true,
   },
-    {
-      name: "Seele",
-      image: "https://your-s3-bucket.s3.amazonaws.com/characters/seele.jpg",
-      limited5Cost: true,
-    },
-    {
-      name: "Blade",
-      image: "https://your-s3-bucket.s3.amazonaws.com/characters/blade.jpg",
-      limited5Cost: true,
-    },
-    {
-      name: "Asta",
-      image: "https://your-s3-bucket.s3.amazonaws.com/characters/asta.jpg",
-      limited5Cost: false,
-    },
-    // ...add more characters
+  {
+    name: "Aglaea",
+    image: "/units/aglaea.jpg",
+    limited5Cost: true,
+  },
+  {
+    name: "Anaxa",
+    image: "/units/anaxa.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Argenti",
+    image: "/units/argenti.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Arlan",
+    image: "/units/acheron.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Asta",
+    image: "/units/asta.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Aventurine",
+    image: "/units/aventurine.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Bailu",
+    image: "/units/bailu.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Black Swan",
+    image: "/units/black_swan.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Blade",
+    image: "/units/blade.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Boothill",
+    image: "/units/boothill.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Bronya",
+    image: "/units/bronya.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Castorice",
+    image: "/units/castorice.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Clara",
+    image: "/units/clara.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Dan Heng",
+    image: "/units/dan_heng.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Dan Heng • Imbibitor Lunae",
+    image: "/units/dhil.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Dr. Ratio",
+    image: "/units/dr_ratio.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Feixiao",
+    image: "/units/feixiao.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Firefly",
+    image: "/units/firefly.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Fu Xuan",
+    image: "/units/fuxuan.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Gallagher",
+    image: "/units/gallagher.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Gepard",
+    image: "/units/gepard.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Guinaifen",
+    image: "/units/guinaifen.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Hanya",
+    image: "/units/hanya.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Herta",
+    image: "/units/herta.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Himeko",
+    image: "/units/himeko.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Hook",
+    image: "/units/hook.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Huohuo",
+    image: "/units/huohuo.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Hyacine",
+    image: "/units/hyacine.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Jade",
+    image: "/units/jade.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Jiaoqiu",
+    image: "/units/jiaoqiu.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Jing Yuan",
+    image: "/units/jingyuan.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Jingliu",
+    image: "/units/jingliu.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Kafka",
+    image: "/units/kafka.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "lingsha",
+    image: "/units/lingsha.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Luka",
+    image: "/units/luka.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Luocha",
+    image: "/units/luocha.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Lynx",
+    image: "/units/lynx.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "March 7th",
+    image: "/units/m7.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "March 7th • The Hunt",
+    image: "/units/m8.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Misha",
+    image: "/units/misha.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Moze",
+    image: "/units/moze.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Mydei",
+    image: "/units/mydei.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Natasha",
+    image: "/units/natasha.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Pela",
+    image: "/units/pela.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Qingque",
+    image: "/units/qingque.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Rappa",
+    image: "/units/rappa.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Robin",
+    image: "/units/robin.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Ruan Mei",
+    image: "/units/ruanmei.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Sampo",
+    image: "/units/sampo.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Seele",
+    image: "/units/seele.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Serval",
+    image: "/units/serval.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Silver Wolf",
+    image: "/units/silverwolf.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Sparkle",
+    image: "/units/sparkle.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Sunday",
+    image: "/units/sunday.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Sushang",
+    image: "/units/sushang.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "The Herta",
+    image: "/units/therta.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Tingyun",
+    image: "/units/tingyun.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Fugue",
+    image: "/units/fugue.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Topaz & Numby",
+    image: "/units/topaz.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Trailblazer • Destruction",
+    image: "/units/dmc.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Trailblazer • Harmony",
+    image: "/units/hmc.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Trailblazer • Preservation",
+    image: "/units/fmc.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Trailblazer • Remembrance",
+    image: "/units/rmc.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Tribbie",
+    image: "/units/tribbie.webp",
+    limited5Cost: true,
+  },
+  {
+    name: "Welt",
+    image: "/units/welt.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Xueyi",
+    image: "/units/xueyi.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Yanqing",
+    image: "/units/yanqing.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Yukong",
+    image: "/units/yukong.webp",
+    limited5Cost: false,
+  },
+  {
+    name: "Yunli",
+    image: "/units/yunli.webp",
+    limited5Cost: true,
+  }
   ];
   
   export default characters;
